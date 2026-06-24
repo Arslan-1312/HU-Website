b9a19b78ac29a13115dde70bac8c9369bde16d83
Arslan-1312 <arslaniqbal1312@gmail.com>
2026-06-24 22:24:52 +0500

    Use relative base path for asset resolution

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/HU-Website/',
+  base: './',
   plugins: [react()],
   build: {
     rollupOptions: {
